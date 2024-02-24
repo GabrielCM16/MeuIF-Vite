975bf371f88148bbf39cde22623ad0878672224c
GabrielCM16 <gabrielcostademoraes13@gmail.com>
2024-02-24 14:21:35 -0300

    first commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCG_UJeiEYlK8iaLUCE1eLcpuz-vNT-I0E",
+  authDomain: "meuif-15823.firebaseapp.com",
+  projectId: "meuif-15823",
+  storageBucket: "meuif-15823.appspot.com",
+  messagingSenderId: "686398009761",
+  appId: "1:686398009761:web:3dcd68e2414c6400e15be0",
+  measurementId: "G-T5FK1MCC4B"
+};
+
+
+export const app = initializeApp(firebaseConfig);
---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import imagemGIF from './assets/logogif.gif';
+
+
+function App() {
+  return (
+    <div className="background-container">
+      <div className="mx-5 p-0 px-0 rounded-2 col-sm-12 col-md-6">
+        <div className="mb-4">
+        <img id="gif" src={imagemGIF} alt="GIF" className="bg-gif" style={{ width: '50%', height: 'auto' }} />
+          <h3>Entrar</h3>
+          <p className="mb-4 text-dark">Realize o login para desfrutar dos recursos do MeuIF.</p>
+        </div>
+
+        <form action="#" method="post">
+
+          <div className="form-group first">
+            <label htmlFor="username">Email</label>
+            <input type="text" className="form-control transparent-input" placeholder="seu@email.com" id="email" />
+            <div id="avisoEmail" style={{ display: 'none', color: 'red' }}>Email inválido</div>
+          </div>
+
+          <div className="form-group last mb-3">
+            <label htmlFor="password">Senha</label>
+            <input type="password" className="form-control transparent-input" placeholder="senha" id="password" />
+            <div id="avisoPassword" style={{ display: 'none', color: 'red' }}>Senha inválida</div>
+          </div>
+
+          <div className="d-flex mb-5 align-items-center">
+            <label className="control control--checkbox mb-0"><span className="caption">Lembre-me</span>
+              <input type="checkbox" defaultChecked />
+              <div className="control__indicator"></div>
+            </label>
+            <span className="ml-auto"><a href="#" className="forgot-pass">Esqueci minha senha</a></span>
+          </div>
+
+          <input type="button" value="Entrar" className="btn btn-block btn-primary" />
+
+          <span className="d-block text-center my-4 text-muted">&mdash; ou &mdash;</span>
+
+          <div className="social-login">
+
+          <a href="#" className="google btn d-flex justify-content-center align-items-center">
+            <i className="material-icons mr-3">account_circle</i> Continuar com Google
+          </a>
+
+
+            <a href="cadastro.html" className="d-block text-center my-4 text-muted">Não possui conta? Cadastre-se</a>
+
+          </div>
+
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
