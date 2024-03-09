37c1a5cb297b05aaafa403135001b5d5ae7fa091
GabrielCM16 <gabrielcostademoraes13@gmail.com>
2024-03-08 21:30:58 -0300

    carteirinha atualizada com loading

--- src/components/carteirinha/carteirinha.tsx
+++ src/components/carteirinha/carteirinha.tsx
@@ -20,22 +20,33 @@ function Carteirinha() {
     const [nome, setNome] = useState('');
     const [turma, setTurma] = useState('');
 
+    const gif_loader_src:string = "https://i.gifer.com/ZKZg.gif"; 
+
+
     // Função para gerar o QR Code
-    function GerarQRCode() {
+    async function GerarQRCode() {
         const matriculaLocal = recuperarDadosLocalStorage("matricula");
         if (matriculaLocal != null) {
             setMatricula(matriculaLocal);
             mostrarDados();
+                        
+            // Coerção de tipo para indicar que o elemento é um HTMLImageElement
+            const qrCodeImage = document.querySelector('#QRCodeImage') as HTMLImageElement;
+            qrCodeImage.src = gif_loader_src;
+            qrCodeImage.width = 70;
+
             const GoogleChartAPI = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chld=H&chl=';
             const conteudoQRCode = GoogleChartAPI + matriculaLocal;
 
-            // Coerção de tipo para indicar que o elemento é um HTMLImageElement
-            const qrCodeImage = document.querySelector('#QRCodeImage') as HTMLImageElement;
             if (qrCodeImage) {
                 qrCodeImage.src = conteudoQRCode;
+                qrCodeImage.width = 300;
+
             } else {
                 console.error('Elemento #QRCodeImage não encontrado.');
             }
+
+
         } else {
             console.error('Matrícula não encontrada no localStorage.');
             () => logout();
@@ -82,6 +93,8 @@ function Carteirinha() {
             <div className="m-0 p-0 px-0 rounded-2">
                 <div className="mb-4">
                     <img id="gif" src={imagemIF} alt="GIF" className="bg-gif" style={{ width: '100%', height: 'auto' }} />
+                    
+                    {/* <img id="gif" src={gif_loader_src} alt="GIF" className="bg-gif" style={{ width: '30%', height: 'auto' }} /> */}
 
                     <div className="d-flex mb-5 flex-column align-items-start justify-content-between">
                         <p></p>
@@ -96,7 +109,7 @@ function Carteirinha() {
                         </div>
                     </div>
                     <div>
-                        <img id="QRCodeImage" src="" alt="carteirinha qr code" style={{ width: '100%', height: 'auto' }} />
+                        <img id="QRCodeImage" src="" alt="carteirinha qr code" style={{ height: 'auto' }} />
                     </div>
                     <div className='mt-5 '>
                         <button type="button" className="btn btn-block p-2 px-5" style={{ backgroundColor: '#5DC1B9' }} onClick={() => logout()}>Sair</button>
